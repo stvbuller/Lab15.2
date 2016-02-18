21cdfa1d2d1835783d657386dd9ae5196216df12
Steven Buller <steven@Stevens-MacBook-Pro.local>
2016-02-17 21:28:37 -0500

    added password hashing

--- server.js
+++ server.js
@@ -8,15 +8,63 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
+var bcrypt = require('bcryptjs');
 
 var sequelize = new Sequelize('auth2_db', 'root');
 
 var PORT = process.env.NODE_ENV || 8090;
 
-var User = sequelize.define('User', {
-  name: Sequelize.STRING,
-  number: Sequelize.STRING,
+
+var Message = sequelize.define('Message', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [1, 15],
+        msg: "Please enter a name that isn't too long"
+      },
+      is: ["^[a-z]+$",'i']
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    validate: {
+      min: 8
+    },
+    allowNull: false
+  },
 });
+
+
+//wolfgang's model
+// var Message = sequelize.define('Message', {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     validate: {
+//       len: {
+//         args: [1, 15],
+//         msg: "Please enter a name that isn't too long"
+//       },
+//       is: ["^[a-z]+$",'i']
+//     }
+//   },
+//   phone: {
+//     type: Sequelize.INTEGER,
+//     validate: {
+//       isInt: true,
+//     },
+//     allowNull: false
+//   },
+//   message: {
+//     type: Sequelize.TEXT,
+//     validate: {
+//       len: [5, 500],
+//       allowNull: false
+//     }
+//   }
+// });
 
 app.get('/', function (req, res) {
     res.render('home', {
@@ -24,11 +72,16 @@ app.get('/', function (req, res) {
     });
 });
 
-app.post('/user', function(req, res) {
-  User.create(req.body).then(function(user){
-    res.redirect('/');
-  }).catch(function(err) {
-    res.redirect('/msg=' + err.message);
+app.post('/message', function(req, res) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hashedPassword) {
+      Message.create({
+        name: req.body.name,
+        password: hashedPassword
+      }).then(function() {
+        console.log("SAVED!");
+      });
+    });
   });
 });
 
